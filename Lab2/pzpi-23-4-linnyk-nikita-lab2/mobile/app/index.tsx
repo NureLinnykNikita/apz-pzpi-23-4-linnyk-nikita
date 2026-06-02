ba53787b2d7eae1e517a48eb6b0fb3962d122090
NureLinnykNikita <165603257+NureLinnykNikita@users.noreply.github.com>
2026-06-02 16:56:40 +0300

    Delete Lab2/pzpi-23-4-linnyk-nikita-lab2/mobile directory

--- Lab2/pzpi-23-4-linnyk-nikita-lab2/mobile/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Redirect } from 'expo-router';
-import { useAuthStore } from '../src/store/auth.store';
-
-export default function Index() {
-  const accessToken = useAuthStore((s) => s.accessToken);
-  return <Redirect href={accessToken ? '/(app)' : '/(auth)/login'} />;
-}
